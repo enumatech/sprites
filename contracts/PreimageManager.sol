0f79275a6f1f0f88695639de767bb15760209e64
Tamas Herman <tamas.herman@enuma.io>
2018-12-17 17:55:17 +0800

    Upgrade to solc 0.5.1
    to make sure we are not hitting any compiler errors.

--- contracts/PreimageManager.sol
+++ contracts/PreimageManager.sol
@@ -7,14 +7,14 @@ import "./PreimageManagerInterface.sol";
 contract PreimageManager is PreimageManagerInterface {
     mapping (bytes32 => uint256) preimageMap;
 
-    function submitPreimage(bytes32 preimage) public {
+    function submitPreimage(bytes32 preimage) external {
         bytes32 hash = keccak256(abi.encodePacked(preimage));
         if (preimageMap[hash] == 0) {
             preimageMap[hash] = block.number;
         }
     }
 
-    function revealedBefore(bytes32 hash, uint256 expectedBlock) public view returns (bool) {
+    function revealedBefore(bytes32 hash, uint256 expectedBlock) external view returns (bool) {
         uint256 actualBlock = preimageMap[hash];
 
         return (actualBlock > 0 && actualBlock <= expectedBlock);
--- contracts/PreimageManagerInterface.sol
+++ contracts/PreimageManagerInterface.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 
 interface PreimageManagerInterface {
     function submitPreimage(bytes32 preimage) external;
-    function revealedBefore(bytes32 hash, uint256 expectedBlock) external returns (bool);
+    function revealedBefore(bytes32 hash, uint256 expectedBlock) external view returns (bool);
 }
 
 
