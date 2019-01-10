6e1c6f14cd2c5e140945d30e40a64b4aeb08efae
Tamas Herman <tamas.herman@enuma.io>
2019-01-10 13:59:26 +0800

    Handle sync and async `unexpected` exceptions equivalently

--- examples/paywall/test/custom-tap.js
+++ examples/paywall/test/custom-tap.js
@@ -1,15 +1,18 @@
 const tap = require('tap')
-const expect = require('unexpected')
+const unexpected = require('unexpected')
 
-tap.Test.prototype.addAssert('expect', 1,
-    function ([subject, assertionName, ...rest], message, extra) {
-        try {
-            expect(subject, assertionName, ...rest)
-        } catch (e) {
-            return this.fail(message, {...extra, found: '', wanted: e.message})
-        }
-        return this.pass(message, extra)
-    })
+tap.Test.prototype.expect = async function (unexcpectedArgs, message, extra) {
+    const t_resolves = e =>
+        this.resolves(e, message, {...extra, found: '', wanted: e.message})
+
+    try {
+        return unexpected(...unexcpectedArgs).catch(t_resolves)
+    } catch (e) { // Handle synchronous throw, when `.catch()` cannot run
+        return t_resolves(e)
+    }
+}
+
+tap.Test.prototype.expect.it = unexpected.it.bind(unexpected)
 
 tap.reporter = reporterName =>
     tap.pipe(new (require('tap-mocha-reporter'))(reporterName))
--- examples/paywall/test/unexpected-demo-flow.js
+++ examples/paywall/test/unexpected-demo-flow.js
@@ -10,20 +10,22 @@ const Reader = require('../reader.js')
 const balance = async ({sprites}) =>
     (await Sprites.tokenBalance(sprites)).tokenBalance
 
-t.test('Paywall demo flow', {bail: true}, async t => {
+t.test('Paywall demo flow', {bail: true, timeout: 10 * 1000}, async t => {
     const Article1 = {
         id: "aId-1", price: 10, title: 'Article 1',
         content: `Article 1 content`, blurb: 'Article 1 blurb'
     }
     const Articles = [Article1]
     const ArticleDB = indexBy(prop('id'), Articles)
-    const web3Provider = makeProvider()
+    const deployerWeb3Provider = makeProvider()
     const {accounts: {ALICE, BOB}, ...spritesTemplate} =
-        await Sprites.testDeploy({web3Provider})
+        await Sprites.testDeploy({web3Provider: deployerWeb3Provider})
+    const web3Provider = makeProvider()
     const publisher = Publisher.make({
         db: ArticleDB,
         sprites: thread({
                 ...spritesTemplate,
+                web3Provider,
                 ACTOR_NAME: 'Publisher',
                 ownAddress: BOB,
                 offChainReg: new OffChainRegistry({ownAddress: BOB})
@@ -67,17 +69,20 @@ t.test('Paywall demo flow', {bail: true}, async t => {
     const {paymentReceipt} = await Publisher.processPayment(payment, publisher)
     const {receipt} = await Reader.processReceipt(paymentReceipt, reader)
     const {article: paidArticle} = await Publisher.getArticle(receipt, publisher)
-    t.expect([paidArticle, 'to satisfy', {content}],
+    await t.expect([paidArticle, 'to satisfy', {content}],
         `Article content is readable`)
-    await t.resolveMatch(Reader.library(reader), {[article.id]: receipt},
-        `Article is saved in the Reader's library`)
+    await t.expect([
+        Reader.library(reader), 'to be fulfilled with',
+        {[article.id]: receipt}
+    ], `Article is saved in the Reader's library`)
 
     const publisherWithdraw = async t => {
         await t.resolves(Publisher.publisherWithdraw(chId, publisher),
             `Publisher withdraws`)
-        await t.resolveMatch(
-            balance(publisher), publisherOpeningBalance + article.price,
-            `Publisher receives article price`)
+        await t.expect([
+            balance(publisher), 'to be fulfilled with',
+            publisherOpeningBalance + article.price
+        ], `Publisher receives article price`)
     }
 
     const readerWithdraw = async t => {
@@ -86,8 +91,10 @@ t.test('Paywall demo flow', {bail: true}, async t => {
             await Publisher.readerWithdraw(withdrawalRequest, publisher)
         await t.resolves(Reader.withdraw(withdrawal, reader),
             `Reader withdraws`)
-        await t.resolveMatch(balance(reader), readerOpeningBalance - article.price,
-            `Reader balance is reduced by article price`)
+        await t.expect([
+            balance(reader), 'to be fulfilled with',
+            readerOpeningBalance - article.price
+        ], `Reader balance is reduced by article price`)
     }
 
     await t.test(`Withdraw without round increment`, async t => {
